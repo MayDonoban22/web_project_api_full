aebca159d5d27d683d91685c98b525c4519f0235
May Donoban <rppride_2411@hotmail.com>
2025-09-02 00:15:34 -0500

    vercel_new_url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/web_project_around_auth">
+    <BrowserRouter basename="/web_project_around_api_full">
       <App />
     </BrowserRouter>
   </React.StrictMode>
