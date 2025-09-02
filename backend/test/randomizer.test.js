1aa86e39d581aa23985473d5ab3dfe57b0514f9a
May Donoban <rppride_2411@hotmail.com>
2025-09-01 22:15:36 -0500

    frontend: vercel_deploy

--- backend/test/randomizer.test.js
+++
@@ -1,7 +0,0 @@
-const randomizer = require("../randomizer");
-
-test("Genera números aleatorios entre 10 y 80 (no incluidos)", () => {
-  expect(typeof randomizer()).toBe("number");
-  expect(randomizer()).toBeGreaterThan(10);
-  expect(randomizer()).toBeLessThan(80);
-});
